caac781d2b24931887a2ecc0046283738d8969de
Mariola Furdak <furdakmariola77@gmail.com>
2025-01-24 22:50:05 +0000

    Dzień 4

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, Mariola!");
+﻿string name = "Ewa";
+string gender = "Female";
+int age = 30;
+
+if (gender == "Fimale" && age < 30)
+{
+    Console.WriteLine("Konieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (gender != "Fimale" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine();
+}
